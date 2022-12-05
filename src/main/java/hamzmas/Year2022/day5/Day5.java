3a39256903096d11acbe6659600a06a4aac864af
HamzMas <32570820+HamzMas@users.noreply.github.com>
2022-12-05 12:41:31 +0000

    Day 5 start

---
+++ src/main/java/hamzmas/Year2022/day5/Day5.java
@@ -0,0 +1,17 @@
+package hamzmas.Year2022.day5;
+
+import java.util.List;
+import static hamzmas.Year2022.util.ReadFile.getLinesFromFile;
+
+public class Day5 {
+
+    static List<String> lines = getLinesFromFile("src/main/resources/Year2022/day5input.txt");
+
+    public static int getSumOfPairsInRange() {
+        int sum = 0;
+        for(String line : lines) {
+
+        }
+        return sum;
+    }
+}
